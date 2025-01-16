f3d09b2e4e38c919bd2be75a21af86a295d20222
Nemanja <andric91@hotmail.com>
2025-01-16 01:47:03 +0100

    Core prefix for libs

--- libs/auto-form/src/lib/auto-form/auto-form.component.ts
+++ libs/auto-form/src/lib/auto-form/auto-form.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'lib-auto-form',
+  selector: 'core-auto-form',
   imports: [CommonModule],
   templateUrl: './auto-form.component.html',
   styleUrl: './auto-form.component.scss',
